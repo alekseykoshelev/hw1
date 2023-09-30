aa7101662bbfb3f07b3033d674f344fdc4f31c6b
Aleksey Koshelev <koshelev.alexej@gmail.com>
2023-09-30 12:17:23 +0400

    Домашка готова!

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!jhbsadjhbasdjhbasjdhbajhsdb");
-
+        // моя домашка готова!
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
@@ -13,7 +13,7 @@ public class Main {
             // for you, but you can always add more by pressing Cmd+F8.
             System.out.println("i = " + i);
         }
-
+        // домашка точно сделана!
         // commit
     }
 }
